e0375c40bb1c17611f2c3a8e03d5ee61c47e4264
Chris Kjellqvist <chriskjellqvist@gmail.com>
2015-10-11 17:28:05 -0500

    window listener now fetches value of a text box on close.

--- src/testing/test.java
+++ src/testing/test.java
@@ -1,11 +1,14 @@
 package testing;
 
 import javax.swing.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 /**
  * Created by chris on 10/11/15.
  */
 public class test {
+    private static String a = "asdf";
     private JTextField NWx;
     private JTextField NWy;
     private JTextField SEx;
@@ -17,9 +20,18 @@ public class test {
     public static void main(String[] args) {
         JFrame frame = new JFrame("test");
         frame.setContentPane(new test().something);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.addWindowListener(new WindowAdapter() {
+            public void windowClosed(WindowEvent e) {
+                a = new test().NWx.getText();
+                System.out.println(a);
+            }
+
+        });
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
 
     }
+
+
 }
