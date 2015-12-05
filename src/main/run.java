0453fa22f5b4f1febfd2c6c32bef756a6c45bbd5
Chris Kjellqvist <chriskjellqvist@gmail.com>
2015-12-05 13:21:25 -0600

    For debugging.

--- src/main/run.java
+++ src/main/run.java
@@ -9,24 +9,26 @@ import java.util.Scanner;
  * Control class that will be run when the jar is run.
  */
 public class run {
-    public static void main(String[] args) throws Exception {
-        if (!fileExists(new File("settings.txt"))) {
-            start.run();
-        } else if (!fileExists(new File("api_key.txt"))) {
-            alert.display("Make your api_key.txt please.");
-        } else {
-            gatherData.run();
+    public static void main(String[] args) {
+        if (!(fileExists(new File("./settings.txt")))) {
+            alert.display("couldn't find settings.");
+            //start.run();
         }
+        if (!(fileExists(new File("./api_key.txt")))) {
+            alert.display("Make your api_key.txt pleaseaa.");
+        }
+
+        /*else {
+            gatherData.run();
+        }*/
     }
 
     public static boolean fileExists(File a) {
         try {
             Scanner sc = new Scanner(a);
-            sc.close();
-        } catch (FileNotFoundException e) {
+        } catch (FileNotFoundException exc) {
             return false;
         }
         return true;
     }
-
 }
